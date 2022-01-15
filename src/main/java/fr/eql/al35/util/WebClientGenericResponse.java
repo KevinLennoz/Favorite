218cf724d3e5c7eba5b956765333af9aa2773768
Kevin Lennoz <lennoz.kevin@hotmail.fr>
2022-01-15 13:18:32 +0100

    prise en compte des try catch generic

--- src/main/java/fr/eql/al35/util/WebClientGenericResponse.java
+++ src/main/java/fr/eql/al35/util/WebClientGenericResponse.java
@@ -1,55 +1,80 @@
 package fr.eql.al35.util;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.core.publisher.Mono;
 
 public class WebClientGenericResponse {
+	
+	private static final Logger logger = LoggerFactory.getLogger(WebClientGenericResponse.class);
 
 	public static <T> List<T> getListResponse(WebClient webclient, String endpoint, T t){
-		return (List<T>) webclient.get()
-				.uri(endpoint)
-				.accept(MediaType.APPLICATION_JSON)
-				.acceptCharset(StandardCharsets.UTF_8)
-				.retrieve()
-				.bodyToFlux(t.getClass())
-				.collectSortedList()
-				.block();
+		try {
+			return (List<T>) webclient.get()
+					.uri(endpoint)
+					.accept(MediaType.APPLICATION_JSON)
+					.acceptCharset(StandardCharsets.UTF_8)
+					.retrieve()
+					.bodyToFlux(t.getClass())
+					.collectSortedList()
+					.block();
+		} catch (Exception e) {
+			logger.info(e.getMessage());
+		}
+		return Collections.emptyList();
 	}
 
 	public static <T> T getResponse(WebClient webclient, String endpoint, T t){	
-		return (T) webclient.get()
-				.uri(endpoint)
-				.accept(MediaType.APPLICATION_JSON)
-				.acceptCharset(StandardCharsets.UTF_8)
-				.retrieve()
-				.bodyToMono(t.getClass())
-				.block();
+		try {
+			return (T) webclient.get()
+					.uri(endpoint)
+					.accept(MediaType.APPLICATION_JSON)
+					.acceptCharset(StandardCharsets.UTF_8)
+					.retrieve()
+					.bodyToMono(t.getClass())
+					.block();
+		} catch (Exception e) {
+			logger.info(e.getMessage());
+		}
+		return null;
 	}
 
 	public static <T> T postResponse(WebClient webclient, String endpoint, T t){	
-		return (T) webclient.post()
-				.uri(endpoint)
-				.body(Mono.just(t), t.getClass())
-				.accept(MediaType.APPLICATION_JSON)
-				.acceptCharset(StandardCharsets.UTF_8)
-				.retrieve()
-				.bodyToMono(t.getClass())
-				.block();
+		try {
+			return (T) webclient.post()
+					.uri(endpoint)
+					.body(Mono.just(t), t.getClass())
+					.accept(MediaType.APPLICATION_JSON)
+					.acceptCharset(StandardCharsets.UTF_8)
+					.retrieve()
+					.bodyToMono(t.getClass())
+					.block();
+		} catch (Exception e) {
+			logger.info(e.getMessage());
+		}
+		return null;
 	}
 	public static <T> T putResponse(WebClient webclient, String endpoint, T t){	
-		return (T) webclient.put()
-				.uri(endpoint)
-				.body(Mono.just(t), t.getClass())
-				.accept(MediaType.APPLICATION_JSON)
-				.acceptCharset(StandardCharsets.UTF_8)
-				.retrieve()
-				.bodyToMono(t.getClass())
-				.block();
+		try {
+			return (T) webclient.put()
+					.uri(endpoint)
+					.body(Mono.just(t), t.getClass())
+					.accept(MediaType.APPLICATION_JSON)
+					.acceptCharset(StandardCharsets.UTF_8)
+					.retrieve()
+					.bodyToMono(t.getClass())
+					.block();
+		} catch (Exception e) {
+			logger.info(e.getMessage());
+		}
+		return null;
 	}
 	
 	private WebClientGenericResponse() {
